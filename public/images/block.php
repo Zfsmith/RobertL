c880acf053fe70236d8c3d0475651f184a622f75
Zfsmith <zac2008@aol.com>
2017-08-02 16:39:13 -0500

    fixed some ie stuff

---
+++ public/images/block.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perceptive Investigations</title>
+    <link rel="icon" href="images/favicon.ico" type="image/ico">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+	  <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery.js"></script>
+  </head>
+  <body>
+    <img class="logo-block" src="images/logo.png">
+  </body>
+</html>
